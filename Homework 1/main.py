d66344b75b6a9cf24517e36e0c5223b468b81a51
annatvo <123956863+annatvo@users.noreply.github.com>
2023-06-17 05:15:55 -0500

    Add files via upload

---
+++ Homework 1/main.py
@@ -0,0 +1,14 @@
+# Anh Vo
+# PSID: 2037824
+# Lab 1.20
+
+user_num1 = int(input('Enter integer:\n'))
+num_square = user_num1 * user_num1
+num_cube = user_num1 * user_num1 * user_num1
+print('You entered:', user_num1)
+print(user_num1,'squared is', user_num1 ** 2)
+print('And', user_num1, 'cubed is', user_num1 ** 3,'!!')
+user_num2 = int(input('Enter another integer:\n'))
+print(user_num1,'+', user_num2,'is', user_num1 + user_num2)
+print(user_num1,'*', user_num2,'is', user_num1 * user_num2)
+
