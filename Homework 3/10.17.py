acd051bb076f3f0190642a45842cf217155c7951
annatvo <123956863+annatvo@users.noreply.github.com>
2023-07-15 21:18:24 -0500

    Create 10.17.py

---
+++ Homework 3/10.17.py
@@ -0,0 +1,33 @@
+#Anh Vo
+#2037824
+#Lab 10.17
+
+class ItemToPurchase:
+    def __init__(self):
+        self.item_name = 'none'
+        self.item_price = 0.0
+        self.item_quantity = 0
+
+    def print_item_cost(self):
+        print(f'{self.item_name} {self.item_quantity} @ ${self.item_price} = ${self.item_price * self.item_quantity}')
+
+if __name__ == "__main__":
+    # Get Items
+    print('Item 1')
+    item1 = ItemToPurchase()
+    item1.item_name = input("Enter the item name:\n")
+    item1.item_price = int(input("Enter the item price:\n"))
+    item1.item_quantity = int(input("Enter the item quantity:\n"))
+
+    print('\nItem 2')
+    item2 = ItemToPurchase()
+    item2.item_name = input("Enter the item name:\n")
+    item2.item_price = int(input("Enter the item price:\n"))
+    item2.item_quantity = int(input("Enter the item quantity:\n"))
+
+    # Total Cost
+    print('\nTOTAL COST')
+    item1.print_item_cost()
+    item2.print_item_cost()
+    total_cost = (item1.item_price * item1.item_quantity) + (item2.item_price * item2.item_quantity)
+    print(f"\nTotal: ${total_cost}")
